029a169513b2a41d961cb6eff55fcf154fe75bc4
Robert Peck <rspeckpfs@gmail.com>
2017-08-30 22:12:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+  i = 0
+  output = [] #the coursework said Array.new was the preferred method, but I've only ever seen [] in practice.
+  while i < array.length
+    output << (yield array[i]) #why doesnt this work without parentheses? Just curious about the underlying mechanics.
+    i = i + 1
+  end
+  output
+end
